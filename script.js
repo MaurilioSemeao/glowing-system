69a5a86fa7abba5e98e08eea0bc5048a54ae98b3
MaurilioSemeao <106936169+MaurilioSemeao@users.noreply.github.com>
2022-08-10 01:19:09 -0300

    adiciona funções par pedir texto e exibir na tela referente ao exercicio n1 n2
     - javaScript

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+ const pedeNome = () =>{
+    let nome = "Olá " + prompt("Digite seu Nome")
+    let texto  = document.getElementById('nome')
+    texto.innerHTML = nome;
+ }
+
+const insereFrase = () =>{ 
+    const texto = document.getElementById('texto')
+    const btnh3 = document.getElementById('btntxt');
+    btnh3?.addEventListener('click',()=>{
+       let recebeTexto = prompt("Digite um Texto");
+       texto.innerHTML = recebeTexto;
+    });
+
+}
+
+
+ function iniciaEventos(){ 
+     pedeNome()
+     insereFrase();
+     
+ }
+
+ 
+window.addEventListener('load', iniciaEventos)
+//  window.addEventListener('load',(e)=>{
+//         iniciaEventos();
+//  })
+
+
